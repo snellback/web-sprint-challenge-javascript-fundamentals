92856c5336976c0d5ff96865526ca86a4eb863bb
Harrison Snellback <harrison.snellback@gmail.com>
2022-02-18 18:40:09 -0600

    Completed tasks 1-2 in the index.js file.

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ myFunction();
 
 //🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀: 
 
-
+/* 
+The nested function can access the variable "internal" because it is a child function of the "myFunction" parent and can access the parent scope.
+*/
 
 
 
@@ -28,11 +30,15 @@ myFunction();
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
-
+function summation(num) {
+  let counter = 0;
+  for (let i = 0; i <= num; i++){
+    counter += i;
   }
+  return counter;
+}
  
+console.log(summation(4));
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
 // Given this zoo data from around the United States, follow the instructions below. Use the specific array methods in the requests below to solve the problems.
